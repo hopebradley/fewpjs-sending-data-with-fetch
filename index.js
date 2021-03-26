9056eec523bdd8d93d8bc099c12a3f3179dd51f9
Hope Bradley <hope.e.bradley@gmail.com>
2021-03-26 12:42:35 -0400

    commit tree fd59e51bdc324c0c0d3157e32d90044678c62ec3

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(userName, userEmail) {
+
+    const configObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    };
+
+    return fetch('http://localhost:3000/users', configObject)
+    .then(resp => resp.json())
+    .then(obj => {
+        const elem = document.createElement('h1');
+        elem.innerHTML = obj.id;
+        return elem;
+    })
+    .then(elem => document.querySelector('body').appendChild(elem))
+    .catch(error => {
+        const errorMessage = document.createElement('h3');
+        errorMessage.innerHTML = error.message;
+        document.querySelector('body').appendChild(errorMessage);
+    });
+}
